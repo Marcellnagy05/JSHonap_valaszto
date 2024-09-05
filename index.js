97c323640303fc55b6b320d514da5074c10ee8f6
Marcellnagy05 <marcika863@gmail.com>
2024-09-05 08:49:31 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict"
+
+let dict = {
+    "31": ["January", "March", "May", "July", "August", "October", "December"],
+    "30": ["April", "June", "September", "November"],
+    "28": ["February"]
+}

0a4bd079e92d4ec6ebf3003b49979e686a8a66e2
Marcellnagy05 <marcika863@gmail.com>
2024-09-05 09:15:32 +0200

    debug needed

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 "use strict"
 
-let dict = {
+let Months = {
     "31": ["January", "March", "May", "July", "August", "October", "December"],
     "30": ["April", "June", "September", "November"],
     "28": ["February"]
 }
+
+let Container = document.getElementById("Months_Container");
+let selectedMonth
+Container.addEventListener('change', () =>{
+    selectedMonth = Container.value;
+})
+
+for(let prop in Months){
+    console.log(prop);
+    for(item in prop){
+        console.log(item);
+    }
+}
+
+
+
+
+
+

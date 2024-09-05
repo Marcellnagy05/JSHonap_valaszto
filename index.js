9b57860e09fd345d1eb1ff31fbb97a78d8bce337
Marcellnagy05 <marcika863@gmail.com>
2024-09-05 10:37:23 +0200

    assignment done

--- index.js
+++ index.js
@@ -7,17 +7,46 @@ let Months = {
 }
 
 let Container = document.getElementById("Months_Container");
-let selectedMonth
-Container.addEventListener('change', () =>{
+let main = document.getElementById("main")
+let selectedMonth;
+let selectedDays;
+Container.addEventListener('change', () => {
     selectedMonth = Container.value;
+    Months[31].forEach(element => {
+        if (selectedMonth == element) {
+            console.log(`True: The month has 31 days`);
+            selectedDays = 31
+        }
+    });
+
+    Months[30].forEach(element => {
+        if (selectedMonth == element) {
+            console.log(`True: The month has 30 days`);
+            selectedDays = 30
+        }
+    });
+
+    Months[28].forEach(element => {
+        if (selectedMonth == element) {
+            console.log(`True: The month has 28 days`);
+            selectedDays = 28
+        }
+    });
+    let table = document.createElement('table')
+    for (let i = 0; i < selectedDays; i++) {
+        main.innerHTML = ""
+        let tr = document.createElement('tr')
+        let td = document.createElement('td')
+        let tdTextNode = document.createTextNode(i+1)
+        let textBox = document.createElement('textarea')
+        td.appendChild(tdTextNode)
+        tr.appendChild(td)
+        tr.appendChild(textBox)
+        table.appendChild(tr)
+        main.appendChild(table)
+    }
 })
 
-for(let prop in Months){
-    console.log(prop);
-    for(item in prop){
-        console.log(item);
-    }
-}
 
 
 
